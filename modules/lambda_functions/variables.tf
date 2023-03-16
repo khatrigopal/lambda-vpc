70ded363fb38c3e056a9501f09fb2781df259518
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-16 14:36:47 +0530

    Create variables.tf

---
+++ modules/lambda_functions/variables.tf
@@ -0,0 +1,11 @@
+variable "function_configurations" {
+  type = list(object({
+    function_name   = string
+    handler         = string
+    runtime         = string
+    code_directory  = string
+    role = string
+    
+    memory_size = number
+  }))
+}
