aca0cc9542f6546bffc92cb71503994ebd45ca88
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-16 14:37:29 +0530

    Update variables.tf

--- modules/lambda_functions/variables.tf
+++ modules/lambda_functions/variables.tf
@@ -5,7 +5,9 @@ variable "function_configurations" {
     runtime         = string
     code_directory  = string
     role = string
-    
+    timeout = number
+    subnet_ids    = list(string)
+    security_group_ids = list(string)
     memory_size = number
   }))
 }
