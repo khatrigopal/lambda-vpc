80589134985451a26f40e5230133f99d26c3e3d0
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-23 10:19:25 +0530

    Update main.tf

--- modules/lambda_functions/main.tf
+++ modules/lambda_functions/main.tf
@@ -9,14 +9,11 @@ resource "aws_lambda_function" "lambda_functions" {
   runtime         = each.value.runtime
   timeout                = each.value.timeout
   description            = each.value.description
-  #vpc_config {
-   # subnet_ids         = each.value.subnet_ids
-    #security_group_ids = each.value.security_group_ids
-  #}
+ 
   vpc_config {
     subnet_ids         = var.subnet_ids[each.key]
     security_group_ids = var.security_group_ids[each.key]
-  #}
+  }
   memory_size        = each.value.memory_size
   source_code_hash = filebase64sha256(data.archive_file.lambda_functions[each.key].output_path)
   filename        = data.archive_file.lambda_functions[each.key].output_path
