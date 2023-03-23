b31ac623a4bffb3cd3b90f1924c2846bf53edc18
khatrigopal <35285671+khatrigopal@users.noreply.github.com>
2023-03-23 10:29:18 +0530

    Update variables.tf

--- modules/lambda_functions/variables.tf
+++ modules/lambda_functions/variables.tf
@@ -6,22 +6,16 @@ variable "function_configurations" {
     code_directory  = string
     role = string
     timeout = number
-    #vpc_id = string
-    #subnet_ids    = list(string)
-    #security_group_ids = list(string)
     memory_size = number
   }))
 }
 
-variable "vpc_id" {
-  type = string
-}
-
 variable "subnet_ids" {
-  type = map(list(string))
+  type    = list(string)
+  default = []
 }
 
 variable "security_group_ids" {
-  type = map(list(string))
+  type    = list(string)
+  default = []
 }
-
